f15aea55124973bd32e6533858cf127dd562aa35
Hugo Avila <hugoaf@yahoo.com.mx>
2020-04-17 20:06:32 -0700

    My solution
    https://plume-courageous-cookie.glitch.me/

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const mongo = require("mongodb").MongoClient;
 const app = express();
+const routes = require("./routes.js");
+const auth = require("./auth.js");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "pug");
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+mongo.connect(process.env.MONGO_URI, (err, client) => {
+  let db = client.db("testproject");
+  if (err) {
+    console.log("Database error: " + err);
+  } else {
+    console.log("Successful database connection");
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+    auth(app, db);
+
+    routes(app, db);
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
